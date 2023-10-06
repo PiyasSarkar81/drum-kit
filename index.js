c294018593b7d747c5bfec688da62b98c4e78728
Piyas <Piyas@piyassarkar81@gmail.com>
2023-10-06 16:54:38 +0530

    initial

---
+++ index.js
@@ -0,0 +1,52 @@
+// alert("Hello World!")
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        //console.log(this.innerHTML);
+        makeSound(this.innerHTML);
+        buttenAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttenAnimation(event.key);
+});
+
+function makeSound(key){
+    let audio;
+    switch(key) {
+        case 'w':
+            audio = new Audio("sounds/tom-1.mp3");
+            break;
+        case 'a':
+            audio = new Audio("sounds/tom-2.mp3");
+            break;
+        case 's':
+            audio = new Audio("sounds/tom-3.mp3");
+            break;
+        case 'd':
+            audio = new Audio("sounds/tom-4.mp3");
+            break;
+        case 'j':
+            audio = new Audio("sounds/snare.mp3");
+            break;
+        case 'k':
+            audio = new Audio("sounds/crash.mp3");
+            break;
+        case 'l':
+            audio = new Audio("sounds/kick-bass.mp3");
+            break;
+        default:
+            console.log("Error in sound selection");
+    }
+    audio.play();
+}
+
+function buttenAnimation(key) {
+    let activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
